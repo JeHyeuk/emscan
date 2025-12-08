88160709f98fedda00724904cda6be2e3fbaabc0
JeHyeuk <JeHyeuk.Lee@hyundai-kefico.com>
2025-12-08 13:54:31 +0900

    [JEHYEUK LEE]

--- cannect/can/ascet/CanDiag.py
+++ cannect/can/ascet/CanDiag.py
@@ -1,4 +1,4 @@
-from pyems.ascet import Amd, generateOID
+from pyems.ascet import Amd, generateOID, ProjectIO
 from pyems.candb import CanDb
 from pyems.environ import ENV
 from pyems.logger import Logger
@@ -61,7 +61,14 @@ class Template(Amd):
         self.db = db
         self.messages = list(messages)
         self.n = 1
-
+        if self.name in [
+            "CanHSFPCMD",
+            "CanCVVDD",
+            "CanBMSD_48V",
+            "CanLDCD_48V",
+            "CanMHSGD_48V",
+        ]:
+            self.logger(f'>>> [MANUALLY] ADD SYSCON BREAK TO THIS MODEL')
         return
 
     @staticmethod
@@ -557,11 +564,70 @@ CHANNEL     : {db[f'{self.hw} Channel']}-CAN
 
 
 if __name__ == "__main__":
-    template = Template(
-        CanDb(),
-        r"E:\SVN\model\ascet\trunk\HNB_GASOLINE\_29_CommunicationVehicle\CANInterface\ESC\MessageDiag\CanFDESCD\CanFDESCD.zip",
-        "ESC_01_10ms",
-        "ESC_03_20ms",
-        "ESC_04_50ms",
-    )
-    template.create()
+
+    proj = ProjectIO(r"D:\SVN\model\ascet\trunk\HNB_GASOLINE")
+    comm = proj.bcTree(29)
+    CANDB = CanDb()
+
+    # 단일 모델 뽑을 때 사용하세요.
+    # template = Template(
+    #     CanDb(),
+    #     r"E:\SVN\model\ascet\trunk\HNB_GASOLINE\_29_CommunicationVehicle\CANInterface\ESC\MessageDiag\CanFDESCD\CanFDESCD.zip",
+    #     "ESC_01_10ms",
+    #     "ESC_03_20ms",
+    #     "ESC_04_50ms",
+    # )
+    # template.create()
+
+
+    # 전체 모델 뽑을 때 사용하세요.
+    # 모델 별 메시지는 선언되어 있어야 합니다.
+    # 수기로 수정해야하는 사항을 꼭 파악한 후 반영하세요.
+    # ICE
+    target = {
+        "CanFDABSD": ["ABS_ESC_01_10ms", "WHL_01_10ms", ],
+        "CanFDACUD": ["ACU_01_100ms", "IMU_01_10ms",],
+        "CanFDADASD": ["ADAS_CMD_10_20ms", "ADAS_CMD_20_20ms", "ADAS_PRK_20_20ms", "ADAS_PRK_21_20ms", ],
+        "CanFDBCMD": ["BCM_02_200ms", "BCM_07_200ms", "BCM_10_200ms", "BCM_20_200ms", "BCM_22_200ms", ],
+        "CanFDBDCD": ["BDC_FD_05_200ms", "BDC_FD_07_200ms", "BDC_FD_08_200ms", "BDC_FD_10_200ms", "BDC_FD_SMK_02_200ms", ],
+        "CanBMSD_48V": ["BMS5", "BMS6", "BMS7", ],
+        "CanFDCCUD": ["CCU_OBM_01_1000ms", "CCU_OTA_01_200ms", ],
+        "CanFDCLUD": ["CLU_01_20ms", "CLU_02_100ms", "CLU_18_20ms", ],
+        "CanCVVDD": ["CVVD1", "CVVD2", "CVVD3", "CVVD4", ],
+        "CanFDDATCD": ["DATC_01_20ms", "DATC_02_20ms", "DATC_07_200ms", "DATC_17_200ms", ],
+        "CanFDEPBD": ["EPB_01_50ms", ],
+        "CanFDESCD": ["ESC_01_10ms", "ESC_03_20ms","ESC_04_50ms", ],
+        "CanHSFPCMD": ["FPCM_01_100ms", ],
+        "CanFDFRCMRD": ["FR_CMR_02_100ms","FR_CMR_03_50ms", ],
+        "CanFDHFEOPD": ["L_HFEOP_01_10ms", ],
+        "CanFDHUD": ["HU_GW_03_200ms", "HU_GW_PE_01", "HU_OTA_01_500ms", "HU_OTA_PE_00", "HU_TMU_02_200ms", ],
+        "CanFDICSCD": ["ICSC_02_100ms", "ICSC_03_100ms", ],
+        "CanFDICUD": ["ICU_02_200ms", "ICU_04_200ms", "ICU_05_200ms", "ICU_07_200ms", "ICU_09_200ms", "ICU_10_200ms", ],
+        "CanFDILCUD": ["ILCU_RH_01_200ms", "ILCU_RH_FD_01_200ms", ],
+        "CanLDCD_48V": ["LDC1", "LDC2", ],
+        "CanFDMDPSD": ["MDPS_01_10ms", "SAS_01_10ms",],
+        "CanMHSGD_48V": ["MHSG_STATE1", "MHSG_STATE2", "MHSG_STATE3", "MHSG_STATE4",],
+        "CanFDOPID": ["L_OPI_01_100ms", ],
+        "CanFDPDCD": ["PDC_FD_01_200ms", "PDC_FD_03_200ms", "PDC_FD_10_200ms", "PDC_FD_11_200ms", ],
+        "CanFDSBCMD": ["SBCM_DRV_03_200ms", "SBCM_DRV_FD_01_200ms", ],
+        "CanFDSCUD": ["SCU_FF_01_10ms", ],
+        "CanFDSMKD": ["SMK_05_200ms", ],
+        "CanFDSWRCD": ["SWRC_03_20ms", "SWRC_FD_03_20ms",],
+        "CanFDLTCUD": ["L_TCU_01_10ms", "L_TCU_02_10ms", "L_TCU_03_10ms", "L_TCU_04_10ms",],
+        "CanFDTCUD": ["TCU_01_10ms", "TCU_02_10ms", "TCU_03_100ms",],
+        "CanFDTMUD": ["TMU_01_200ms",],
+    }
+
+    for model, messages in target.items():
+        tree = comm[comm['file'] == f'{model}.zip']
+        if tree.empty:
+            raise KeyError(f'MODEL: {model}.zip NOT FOUND')
+        if len(tree) >= 2:
+            print(tree)
+            n = input(f'DUPLICATED MODELS FOUND, SELECT INDEX OF THE LIST: ')
+            tree = tree.loc[int(n)]
+        else:
+            tree = tree.iloc[0]
+
+        template = Template(CANDB, tree['path'], *messages)
+        template.create()
